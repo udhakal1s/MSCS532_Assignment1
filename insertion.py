bbcf879555c1308eb1ccf8a6ec4940ecd33fb42d
Umesh Dhakal <umeshdhakal84@gmail.com>
2025-09-06 11:48:01 -0600

    Insertion sort that sort the data in decreasing order

--- insertion.py
+++ insertion.py
@@ -7,11 +7,11 @@ def insertionsort(Arraylist):
     for a in range (1,len(Arraylist)):
         key =Arraylist[a]
         b= a-1
-       
+        #Moving the element that are smaller than key to one step to right
         while b>=0 and key > Arraylist[b]:
             Arraylist[b+1]=Arraylist[b]
             b =b -1
-            
+            #Placing the key in the correct position
         Arraylist[b+1]=key
 
 #user input separated by comma
@@ -19,5 +19,5 @@ MyNumbers = input("Enter the numbers separated by commas: ")
 numbers = [int(x.strip()) for x in MyNumbers.split(',')]
 
 insertionsort(numbers)
-
+#Print statement to print the number after they are sorted
 print("List of numbers in decreasing order after insertion sort:", numbers)
